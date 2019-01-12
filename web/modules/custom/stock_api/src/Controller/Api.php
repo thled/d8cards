95f3220f8e5cd4ac83d2a8ade69a29a4043cc025
thled <t.leduec@gmail.com>
2019-01-12 15:23:06 +0100

    D8C-05 Create Service to fetch from API with Guzzle

--- web/modules/custom/stock_api/src/Controller/Api.php
+++ web/modules/custom/stock_api/src/Controller/Api.php
@@ -3,14 +3,31 @@
 namespace Drupal\stock_api\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\stock_api\Service\StockApiClient;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Api extends ControllerBase
 {
+  /** @var \GuzzleHttp\Client $client */
+  private $client;
+
+  public function __construct(StockApiClient $stockApiClient)
+  {
+    $this->client = $stockApiClient;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('stock_api.stock_api_client')
+    );
+  }
+
   public function fetch()
   {
-    $apiUrl = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=BAC';
-    $data = file_get_contents($apiUrl);
-    $results = json_decode($data, true);
+    $results = $this->client->fetch('BAC');
 
     return [
       '#markup' => sprintf(
