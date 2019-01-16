c6853f551b2ddcac17b66b5e00256c2da9c2602f
thled <t.leduec@gmail.com>
2019-01-16 20:20:11 +0100

    D8C-07 Extend QueueWorker to send welcome mail

--- web/modules/custom/queue_reg_mail/src/Plugin/QueueWorker/WelcomeMailSender.php
+++ web/modules/custom/queue_reg_mail/src/Plugin/QueueWorker/WelcomeMailSender.php
@@ -2,7 +2,10 @@
 
 namespace Drupal\queue_reg_mail\Plugin\QueueWorker;
 
+use Drupal\user\Entity\User;
+use Drupal\Core\Mail\MailManager;
 use Drupal\Core\Queue\QueueWorkerBase;
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -16,25 +19,52 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class WelcomeMailSender extends QueueWorkerBase implements
   ContainerFactoryPluginInterface
 {
-  public function __construct()
-  {
+  /** @var MailManager $mailManager */
+  private $mailManager;
+
+  /** @var EntityTypeManager $entityTypeManager */
+  private $entityTypeManager;
+
+  public function __construct(
+    MailManager $mailManager,
+    EntityTypeManager $entityTypeManager
+  ) {
+    $this->mailManager = $mailManager;
+    $this->entityTypeManager = $entityTypeManager;
   }
 
-  /**
-   * {@inheritdoc}
-   */
   public static function create(
     ContainerInterface $container,
     array $configuration,
     $plugin_id,
     $plugin_definition
   ) {
-    return new static();
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    $entityTypeManager = \Drupal::service('entity_type.manager');
+    return new static($mailManager, $entityTypeManager);
   }
 
-  public function processItem($data)
+  public function processItem($userId)
   {
-    $uid = $data->uid;
-    \Drupal::logger('queue_reg_mail')->notice('Mail send to user with id: ' . $uid);
+    /** @var User $user */
+    $user = $this->entityTypeManager->getStorage('user')->load($userId);
+
+    $mail = $this->mailManager->mail(
+      'queue_reg_mail',
+      'welcome_mail',
+      $user->getEmail(),
+      $user->getPreferredLangcode(),
+      ['message' => 'Welcome to D8C, user ' . $user->getAccountName()],
+      null,
+      true
+    );
+
+    $logger = \Drupal::logger('queue_reg_mail');
+    if ($mail['result'] !== true) {
+      $logger->error('There was a problem sending the mail.');
+    }
+    else {
+      $logger->info('The mail has been sent.');
+    }
   }
 }
