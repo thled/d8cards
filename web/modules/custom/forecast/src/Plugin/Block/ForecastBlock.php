5bcb964cca53d9f3f7a0553e69cdb9baca1ae498
Thomas Le Duc <dev@tleduc.de>
2019-05-02 22:13:57 +0200

    D8C-17 Add configuration to block

--- web/modules/custom/forecast/src/Plugin/Block/ForecastBlock.php
+++ web/modules/custom/forecast/src/Plugin/Block/ForecastBlock.php
@@ -3,6 +3,7 @@
 namespace Drupal\forecast\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * @Block(
@@ -10,10 +11,48 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Forecast")
  * )
  */
-class ForecastBlock extends BlockBase {
-  public function build() {
+class ForecastBlock extends BlockBase
+{
+  public function blockForm($form, FormStateInterface $form_state)
+  {
+    parent::blockForm($form, $form_state);
+
+    $config = $this->getConfiguration();
+
+    $form['latitude'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('latitude'),
+      '#default_value' => $config['latitude'] ?? '',
+    ];
+    $form['longitude'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('longitude'),
+      '#default_value' => $config['longitude'] ?? '',
+    ];
+
+    return $form;
+  }
+
+  public function blockSubmit($form, FormStateInterface $form_state)
+  {
+    $this->setConfigurationValue('latitude', $form_state->getValue('latitude'));
+    $this->setConfigurationValue('longitude', $form_state->getValue('longitude'));
+  }
+
+  public function build()
+  {
+    $config = $this->getConfiguration();
+
+    $latitude = $config['latitude'] ?? '0';
+    $longitude = $config['longitude'] ?? '0';
+
     return [
-      '#markup' => $this->t('Forecast is XXXXX with temperature of XXX deg C'),
+      '#markup' => $this->t(
+        'Forecast is @forecast with temperature of @temp deg C',
+      [
+        '@forecast' => 'Clear',
+        '@temp' => '19',
+      ]),
     ];
   }
 }
