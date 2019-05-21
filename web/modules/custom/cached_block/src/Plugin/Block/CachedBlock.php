8597b9ac8dc27bee5b81c373d6fcf672ee4afdd3
Thomas Le Duc <dev@tleduc.de>
2019-05-21 13:11:40 +0200

    D8C-20 Add caching of the block content

--- web/modules/custom/cached_block/src/Plugin/Block/CachedBlock.php
+++ web/modules/custom/cached_block/src/Plugin/Block/CachedBlock.php
@@ -7,6 +7,7 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
 
 /**
  * @Block(
@@ -19,10 +20,16 @@ class CachedBlock extends BlockBase implements ContainerFactoryPluginInterface
   /** @var EntityTypeManager */
   private $entityTypeManager;
 
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $etm)
-  {
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $etm,
+    CacheBackendInterface $cbf
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $etm;
+    $this->cache = $cbf;
   }
 
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self
@@ -31,23 +38,37 @@ class CachedBlock extends BlockBase implements ContainerFactoryPluginInterface
       $configuration,
       $plugin_id,
       $plugin_definition,
-      $container->get('entity_type.manager')
+      $container->get('entity_type.manager'),
+      $container->get('cache.default')
     );
   }
 
   public function build(): array
   {
+    $latestNodes = $this->loadLatestNodes();
+
+    return [
+      '#markup' => $latestNodes,
+    ];
+  }
+
+  private function loadLatestNodes()
+  {
+    if ($cache = $this->cache->get('latest_nodes')) {
+      return $cache->data;
+    }
+
     $storage = $this->entityTypeManager->getStorage('node');
     $ids = $storage->getQuery()->sort('created', 'DESC')->range(0, 5)->execute();
     $nodes = $storage->loadMultiple($ids);
 
     $nodeTitles = '';
-    foreach($nodes as $node) {
+    foreach ($nodes as $node) {
       $nodeTitles = $nodeTitles . ' - ' . $node->title->value;
     }
 
-    return [
-      '#markup' => $nodeTitles,
-    ];
+    $this->cache->set('latest_nodes', $nodeTitles, CacheBackendInterface::CACHE_PERMANENT);
+
+    return $nodeTitles;
   }
 }
