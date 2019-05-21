07986d0dbea81cbcdd6a2a85d80c06e10ffb8281
Thomas Le Duc <dev@tleduc.de>
2019-05-21 12:54:58 +0200

    D8C-20 Change block to display latest 5 nodes

--- web/modules/custom/cached_block/src/Plugin/Block/CachedBlock.php
+++ web/modules/custom/cached_block/src/Plugin/Block/CachedBlock.php
@@ -6,7 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use League\Container\ContainerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @Block(
@@ -14,34 +14,40 @@ use League\Container\ContainerInterface;
  *   admin_label = @Translation("Cached Block")
  * )
  */
-class CachedBlock extends BlockBase //implements ContainerFactoryPluginInterface
+class CachedBlock extends BlockBase implements ContainerFactoryPluginInterface
 {
   /** @var EntityTypeManager */
-  // private $entityTypeManager;
+  private $entityTypeManager;
 
-  // public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $etm) {
-    // parent::__construct($configuration, $plugin_id, $plugin_definition);
-    // $this->entityTypeManager = $etm;
-  // }
-
-  // public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    // return new static(
-      // $configuration,
-      // $plugin_id,
-      // $plugin_definition,
-      // $container->get('entity_type.manager')
-    // );
-  // }
-
-  public function build()
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $etm)
   {
-    // $storage = $this->entityTypeManager->getStorage('node');
-    // $ids = $storage->getQuery()->sort('created', 'DESC')->execute();
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $etm;
+  }
 
-    // $nodes = $storage->loadMultiple($ids);
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): self
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager')
+    );
+  }
+
+  public function build(): array
+  {
+    $storage = $this->entityTypeManager->getStorage('node');
+    $ids = $storage->getQuery()->sort('created', 'DESC')->range(0, 5)->execute();
+    $nodes = $storage->loadMultiple($ids);
+
+    $nodeTitles = '';
+    foreach($nodes as $node) {
+      $nodeTitles = $nodeTitles . ' - ' . $node->title->value;
+    }
 
     return [
-      '#markup' => 'foobar',
+      '#markup' => $nodeTitles,
     ];
   }
 }
