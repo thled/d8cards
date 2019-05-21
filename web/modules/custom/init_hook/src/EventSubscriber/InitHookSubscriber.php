fa1eaf3ab691d9e3bce49d4255105e0ea1f5cfde
Thomas Le Duc <dev@tleduc.de>
2019-05-21 16:29:02 +0200

    D8C-21 Change subscriber to only act on anonymous users

--- web/modules/custom/init_hook/src/EventSubscriber/InitHookSubscriber.php
+++ web/modules/custom/init_hook/src/EventSubscriber/InitHookSubscriber.php
@@ -5,9 +5,18 @@ namespace Drupal\init_hook\EventSubscriber;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Drupal\Core\Session\AccountProxyInterface;
 
 class InitHookSubscriber implements EventSubscriberInterface
 {
+  /** @var AccountProxyInterface */
+  private $user;
+
+  public function __construct(AccountProxyInterface $ap)
+  {
+    $this->user = $ap;
+  }
+
   static function getSubscribedEvents()
   {
     $events[KernelEvents::RESPONSE][] = ['addAccessAllowOriginHeaders'];
@@ -15,8 +24,11 @@ class InitHookSubscriber implements EventSubscriberInterface
     return $events;
   }
 
-  public function addAccessAllowOriginHeaders(FilterResponseEvent $event) {
-    $response = $event->getResponse();
-    $response->headers->set('Access-Control-Allow-Origin', '*');
+  public function addAccessAllowOriginHeaders(FilterResponseEvent $event)
+  {
+    if ($this->user->isAnonymous()) {
+      $response = $event->getResponse();
+      $response->headers->set('Access-Control-Allow-Origin', '*');
+    }
   }
 }
